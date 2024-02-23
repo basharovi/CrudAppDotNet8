9b8eccb3442ab4b72530fb7e3f3cada301962259
Mominur Bashar Ovi <bashar.ovi@outlook.com>
2024-02-23 22:20:26 +0600

    Added Working code

---
+++ CrudAppDotNet8/Models/Users/CreateRequest.cs
@@ -0,0 +1,23 @@
+﻿using CrudAppDotNet8.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudAppDotNet8.Models.Users;
+
+public class CreateRequest
+{
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    [EnumDataType(typeof(Role))]
+    public required string Role { get; set; }
+
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [MinLength(6)]
+    public required string Password { get; set; }
+
+    [Compare("Password")]
+    public required string ConfirmPassword { get; set; }
+}
